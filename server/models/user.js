7b8a85f36b1685b6e2215ec58f6b3461fdf0df6a
KirillTK <kirill99.tkachev@yandex.by>
2019-08-10 22:15:30 +0300

    #4: remove property from user

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,6 @@ const bcrypt = require('bcrypt');
 const UserSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
-  isActivate: {type: Boolean, default: false},
   date: { type: Date, default: Date.now },
 });
 
