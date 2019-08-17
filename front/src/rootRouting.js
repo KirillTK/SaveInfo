866b9e3da034fc9a572bfdb0b5925ead26c82d2a
KirillTK <kirill99.tkachev@yandex.by>
2019-08-17 19:06:15 +0300

    #7: create base pages

--- front/src/rootRouting.js
+++ front/src/rootRouting.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import MainMenu from './components/main-menu';
-import Login from './auth';
 import Profile from './profile';
 import PrivateRoute from './utils/components/private-route';
 import PublicRoute from './utils/components/public-route'
+import SignIn from './auth/containers/sign-in';
+import SignUp from './auth/containers/sign-up';
 
 
 
@@ -19,7 +20,8 @@ const Root = () => {
         <Router>
           <Switch>
             <PrivateRoute path="/profile" component={Profile}/>
-            <PublicRoute path="/login" component={Login}/>
+            <PublicRoute path="/login" component={SignIn}/>
+            <PublicRoute path="/registration" component={SignUp}/>
           </Switch>
         </Router>
       </main>
